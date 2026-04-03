6121e0e64faff4acba1429f6e7f3b47bc35f0512
Konstantin S. <havendv@gmail.com>
2026-04-03 09:10:11 +0400

    feat: Updated OpenAPI spec (#189)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Ultravox/Generated/Ultravox.VoicesClient.VoicesPreviewRetrieve.g.cs
+++ src/libs/Ultravox/Generated/Ultravox.VoicesClient.VoicesPreviewRetrieve.g.cs
@@ -194,7 +194,6 @@ namespace Ultravox
                 try
                 {
                     __response.EnsureSuccessStatusCode();
-
                     var __content = await __response.Content.ReadAsByteArrayAsync(
 #if NET5_0_OR_GREATER
                         cancellationToken
